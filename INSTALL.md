6457654af6a42d56f72e0c47e3a3c4c6f5b9556d
bonada <bonada@rpi.edu>
2021-10-19 23:14:15 -0400

    some frontend

---
+++ INSTALL.md
@@ -0,0 +1,9 @@
+To install all modules needed, open a terminal, and in the top level directory,
+run command "npm install"
+
+Next, run "npm start" and visit localhost:3000 or 127.0.0.1:3000 in any browser
+
+***Changing css may require clearing cache, i recommend using firefox for development in general.
+
+!!!MAKE SURE TO NOT PUSH NODE_MODULES TO THE REPOSITORY!!!
+^^^this should already be handles through the git ignore file (.gitignore)
--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
+// var bodyParser = require('body-parser');
+// const { json } = require('body-parser');
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
+
+// const { MongoClient } = require('mongodb');
+// const uri = "mongodb+srv://TriviaUser:TestUserPass@cluster0.wn0sd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("CodeTrivia").collection("test");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 app.use(express.static(__dirname + '/'));
-
-
 
 app.listen(port, () => {
     console.log('Listening on *: 3000')
