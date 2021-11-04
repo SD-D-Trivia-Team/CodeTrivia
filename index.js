5a34f74ba3f1c7f7bc4e952dec78bd716eac5888
Rileyh2 <rileyh2@rpi.edu>
2021-11-03 20:08:57 -0400

    Worked on github OAuth login implementation
    Created the basic server/client behavior and structure for the GitHub OAuth login.  This includes some reconfiguring of the index.html navbar for the categories page as well as the behavior in server.js, the client behavior in index.js, and some package adjustments to include cors so that we get no CORS errors.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var logged_in = false;
+
 function getQuestions() {
     alert("GETTING");
     fetch("http://localhost:3000/get-questions", {
@@ -7,5 +9,31 @@ function getQuestions() {
             .then(data => {
                 console.log(data);
             });
-  }
+}
 
+$(document).ready(function() {
+    fetch("http://localhost:3000/user", {
+        method: 'GET'
+    })
+    .then(response => response.json()
+    ).then(data => {
+        console.log(data);
+        if (data.userName != ''){
+            console.log('logged in');
+            $("#profile-image-header").attr("src", `http://github.com/${data.userName}.png`);
+            $("#log-text").html("Log Out");
+            $("#log-text").attr("href", `http://localhost:3000/user/logout`);
+            logged_in = true;
+        }
+        else {
+            console.log('logged out');
+            $("#profile-image-header").attr("src", `../images/default_avatar.png`);
+            $("#log-text").html("Log In");
+            $("#log-text").attr("href", `http://localhost:3000/user/login`);
+            logged_in = false;
+        }
+    })
+});
+
+
+$()
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const fetch = require('node-fetch');
 var bodyParser = require('body-parser');
+const cors = require('cors');
 const { json } = require('body-parser');
 const app = express();
 const port = 3000;
 
+const clientID = 'e4baa49643af3729d64f';
+const clientSecret = 'dbfda8a0663d4b5846515b75e7a1d01e163e17f9';
+const user = {
+    userName: '',
+    userId: ''
+};
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -37,10 +46,85 @@ app.get('/get-questions' , async (req, res) =>{
     }
   })
 
+app.get('/user/login', (req, res) => {
+    res.redirect(`https://github.com/login/oauth/authorize?client_id=${clientID}`);
+});
 
+//callback function endpoint for github OAuth: this is the function
+//that gets the user's information based on the code from authorization
+//that then sets the user's username and email into the database.
+function getUser(){
+    return user;
+}
+
+async function setUser(name_value, id_value){
+    user.userName = name_value;
+    user.userId =id_value;
+    return;
+}
+
+async function performUserLookup(user_token){
+    const response = await fetch(`https://api.github.com/user`,  {
+        method: 'GET',
+        headers: {Authorization: `token ${user_token}`, accept: 'application/json'}
+    });
+    const user_json = await response.json();
+    console.log(user_json.login);
+    const set = await setUser(user_json.login, user_json.id);
+    return user_json.login;
+}
+
+async function performUserCallbackFunction(codeName){
+    console.log('Running callback function');
+    console.log(codeName);
+    const body = {
+        client_id: clientID,
+        client_secret: clientSecret,
+        code: codeName
+    };
+    token_val = '';
+
+    //perform a post request to the user
+    const response = await fetch(`https://github.com/login/oauth/access_token?client_id=${body.client_id}&client_secret=${body.client_secret}&code=${body.code}`, {
+        method:'POST',
+        headers: {accept: 'application/json'}
+    }).catch(e => {
+        res.status(400);
+        res.json({
+            success: false,
+            message: e.message 
+        })
+    });
+    const json_obj = await response.json();
+    return json_obj;
+}
+
+//expexted behavior: once all behavior is implemented, this should either
+//create a new user entry in the DB for the user or it should pull information
+//from the DB for the user and set them as the logged in user
+app.get('/user', (req, res) => {
+    res.send(getUser());
+});
+
+app.get('/user/login/callbackfunc', async (req,res) => {
+    console.log(req.query.login);
+    var access_obj = await performUserCallbackFunction(req.query.code);
+    console.log(access_obj);
+    var user_obj = await performUserLookup(access_obj.access_token);
+    console.log(user_obj);
+    console.log(user);
+    //redirect back to the home/about/whatever page is decided as 
+    //a redirect page for the result
+    res.redirect('http://localhost:3000/about');
+});
+
+app.get('/user/logout', (req, res) => {
+    setUser('','');
+    res.redirect('http://localhost:3000/about');
+});
 
 app.use(express.static(__dirname + '/'));
 
 app.listen(port, () => {
-    console.log('Listening on *: 3000')
+    console.log('Listening on *: 3000');
 })
