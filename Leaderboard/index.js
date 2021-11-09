764ccd4c55a34e3d64eaf7723cd2c16c149d07f8
Rileyh2 <rileyh2@rpi.edu>
2021-11-08 19:53:30 -0500

    formatting work and dynamic text
    Added dynamic text for the dates, formatting for a string that will be present if there are no scores for the date, and made the number reflective of the position of the user in the leaderboard.

--- Leaderboard/index.js
+++ Leaderboard/index.js
@@ -32,6 +32,7 @@ class Leaderboard {
     constructor(cat){
         this.category = cat;
         this.scores = [];
+        this.date = new Date();
     }
 
     //viewLeaderboard:
@@ -40,7 +41,7 @@ class Leaderboard {
     viewLeaderboard(){
 
         $("#leaderboard-header").html(`<h3 id="cat-header">${sessionStorage.getItem('cat_full')} Quiz</h3>
-        <h4>Leaderboard - October 27, 2021</h4>`)
+        <h4>Leaderboard - ${this.date.toLocaleDateString('en-US')}</h4>`)
         //set the value of the leaderboard to the specific category and date of the day
         //go through and add in the elements of the leaderboard
         var init_score = true;
@@ -52,10 +53,11 @@ class Leaderboard {
         .then(data => {
             //start by sorting the array based on score values
             data.sort((firstItem, secondItem) => secondItem.score - firstItem.score);
-
+            var leaderboard_ind = 1;
             //Then, go through and append the individual elements using jquery's .append and .html methods
             for(const elem of data){
-                let scoreObj = new Score(elem.name, this.category, elem.Leaderboard);
+                console.log(elem);
+                let scoreObj = new Score(elem.name, this.category, elem.score);
                 this.scores.push(scoreObj);
                 if(init_score){
                     //set score string to use commas
@@ -63,7 +65,7 @@ class Leaderboard {
                     //using .html since we can have an empty message in leadaerboard-container if there are no 
                     //scores, and then .html would overwrite that content in the event that we do have scores
                     $("#leaderboard-container").html(` <div id="top-leaderboard-entry" class="leaderboard-entry row w-75">
-                    <h4 id="top-leaderboard-number auto-marg" class="leaderboard-number col-sm-1">1</h4>
+                    <h4 id="top-leaderboard-number auto-marg" class="leaderboard-number col-sm-1">${leaderboard_ind}</h4>
                     <div class="img-cont col-sm-2 auto-marg">
                         <img id="top-leaderboard-img" class="leaderboard-img auto-marg" src="../images/default_avatar.png" alt="A placeholder for the avatar image">
                     </div>
@@ -72,39 +74,27 @@ class Leaderboard {
                     </div>`);
                     //set initial score to false since there is a top score
                     init_score = false;
+                    leaderboard_ind = leaderboard_ind + 1;
                 }
                 else{
                     //set score string to use commas
                     let score_str = elem.score.toLocaleString('en-US');
                     //append the element
                     $("#leaderboard-container").append(` <div class="leaderboard-entry row w-75">
-                        <h4 class="leaderboard-number auto-marg col-sm-1">2</h4>
+                        <h4 class="leaderboard-number auto-marg col-sm-1">${leaderboard_ind}</h4>
                         <div class="img-cont col-sm-2">
                             <img class="leaderboard-img" src="../images/default_avatar.png" alt="A placeholder for the avatar image">
                         </div>
                         <h5 class="leaderboard-name auto-marg col-sm-6">${elem.name}</h5>
                         <h5 class="leaderboard-score auto-marg col-sm-3">${score_str}</h5>
                     </div>`);
+                    leaderboard_ind = leaderboard_ind + 1;
                 }
             }
         });
     }
 
 }
-
-/*A simple user class:*/
-class User {
-    /*Constructor: Stores username, email, and a list of score objects, one for each category */
-    constructor(name, mail, scoreList) {
-        this.username = name;
-        this.email = mail;
-        this.scores = [];
-        for(const scoreVal in scoreList){
-            this.scores.append(scoreVal);
-        }
-    }
-}
-
 
 $(document).ready(function() {
     //on load, create a new Leaderboard object for the category and then
